398b3939f87c4e6ac57209440de757ad3e69a0dc
Jay VanOrd <jvanord@indasysllc.com>
2017-08-25 22:40:40 -0400

    scaffolded solution

---
+++ TampaBayCoders/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TampaBayCoders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
