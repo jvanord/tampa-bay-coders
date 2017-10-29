743ad7172c90cc6bac23bb179818fbc021fad3cc
Jay VanOrd <jvanord@indasysllc.com>
2017-10-29 13:36:49 -0400

    basic connect test

--- TampaBayCoders/Controllers/ToolsController.cs
+++ TampaBayCoders/Controllers/ToolsController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents.Client;
 using Microsoft.Extensions.Options;
 using TampaBayCoders.Configuration;
+using TampaBayCoders.Data;
+using TampaBayCoders.Models;
 
 namespace TampaBayCoders.Controllers
 {
 	[Route("[controller]")]
-	public class ToolsController : ControllerBase
+	public class ToolsController : Controller
 	{
-		public ToolsController(IOptions<CosmosDbSettings> cosmosDbSettings) : base(cosmosDbSettings) { }
+		private CosmosDbSettings _dbSettings;
+
+		public ToolsController(IOptions<CosmosDbSettings> cosmosDbSettings) : base() { _dbSettings = cosmosDbSettings.Value; }
 
 		[Route("compensation-calculator")]
 		public IActionResult Compensation()
@@ -20,9 +25,32 @@ namespace TampaBayCoders.Controllers
 		}
 
 		[Route("site-diagnostics")]
-		public IActionResult Diagnostics()
+		public async Task<IActionResult> Diagnostics()
 		{
-			return View();
+			var results = await runDiagnosticsAsync();
+			return View(results);
+		}
+
+		private async Task<SiteDiagnosticsViewModel> runDiagnosticsAsync()
+		{
+			var results = new SiteDiagnosticsViewModel();
+			DocumentClient client;
+			results.DatabaseEndpoint = _dbSettings.Endpoint;
+			results.DatabaseId = _dbSettings.DatabaseId;
+			try
+			{
+				client = new DocumentClient(new Uri(_dbSettings.Endpoint), _dbSettings.Key);
+				await client.OpenAsync();
+				results.ConnectionTest = new SiteDiagnosticsViewModel.TestResult();
+			}
+			catch (Exception ex) {
+				results.ConnectionTest = new SiteDiagnosticsViewModel.TestResult
+				{
+					Status = SiteDiagnosticsViewModel.ResultStatus.Fail,
+					Message = ex.Message
+				};
+			}
+			return results;
 		}
 	}
 }
---
+++ TampaBayCoders/Models/SiteDiagnosticsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TampaBayCoders.Models
+{
+	public class SiteDiagnosticsViewModel
+	{
+		public enum ResultStatus { Success, Fail }
+		
+		public class TestResult
+		{
+			public ResultStatus Status { get; set; }
+			public string Message { get; set; }
+		}
+
+		public string DatabaseEndpoint { get; set; }
+		public string DatabaseId { get; set; }
+		public TestResult ConnectionTest { get; internal set; }
+	}
+}
