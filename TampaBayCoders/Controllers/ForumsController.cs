cde2de04bc62499cb9181efb487ff05d030cf963
Jay VanOrd <jvanord@indasysllc.com>
2017-12-05 19:53:02 -0500

    added authorize attribute

--- TampaBayCoders/Controllers/ForumsController.cs
+++ TampaBayCoders/Controllers/ForumsController.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TampaBayCoders.Controllers
 {
+	[Authorize]
     public class ForumsController : Controller
     {
         public IActionResult Index()
